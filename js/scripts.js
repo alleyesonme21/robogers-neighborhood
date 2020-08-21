af8fd48e839b2e8c3fc12c5ea240b77ead554bb0
jd <duverseaujohnny21@gmail.com>
2020-08-21 17:56:51 -0400

    add change the condition

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,21 @@
   //Bussiness Logic
-function neighbor(number) {
-const hood = [];
-for (let i = 0; i <= number; i++) {
- hood.push(i.toString());
+function neighbor(hood) {
+const number = [];
+for (let i = 0; i <= hood; i++) {
+ number.push(i.toString());
  if ((number[i]).match("3")) {
   number[i] = ("Won't you be my neigbor...");
-
-}
+ }
+  
 }
 return number;
 };
 
-  // business logic
+  // Business logic
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
     event.preventDefault();
-    const inputtedNumber = parseInt($("#number").val());
-    const result =neighbor(inputtedNumber) ;
-    $("#outcome").text(result);
-   
+    
+    
   });
 });
