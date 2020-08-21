58e5cc04da7fcf9f6d3730575b402fbc14c9d836
jd <duverseaujohnny21@gmail.com>
2020-08-21 18:39:39 -0400

    my last indentation in js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,17 @@
   //Bussiness Logic
 function neighbor(hood) {
-const number = [];
-for (let i = 0; i <= hood; i++) {
- number.push(i.toString());
- if ((number[i]).match("3")) {
-  number[i] = ("Won't you be my neigbor?.");
- } else if ((number[i]).match("2")) {
- number[i] = ("Boop");
- } else if (number[i].match("1")) {
-   number[i] = ("Beep");
- } else {
-   number[i];
- }
-  
+  const number = [];
+  for (let i = 0; i <= hood; i++) {
+    number.push(i.toString());
+    if ((number[i]).match("3")) {
+      number[i] = ("Won't you be my neigbor?.");
+    } else if ((number[i]).match("2")) {
+      number[i] = ("Boop");
+    } else if (number[i].match("1")) {
+      number[i] = ("Beep");
+    } else {
+      number[i];
+  } 
 }
 return number;
 };
@@ -23,9 +22,6 @@ $(document).ready(function() {
     event.preventDefault();
     const inputtedNumber = $("#number").val();
     let result = neighbor(inputtedNumber);
-    // console.log(result)
-    $("#output").text(result);
-    
-    
+    $("#output").text(result); 
   });
 });
