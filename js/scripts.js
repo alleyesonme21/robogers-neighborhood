0f6812a9b797737dcd27f18899facf8b906b4191
jd <duverseaujohnny21@gmail.com>
2020-08-21 13:10:29 -0400

    Add paramater num, empty array and for loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,17 @@
   //Bussiness Logic
-function neighborHood() { 
+function neighborHood(num) {
+let array = [];
+for (let i = 0; i <= num; i = i + 1) {
+ 
+}
 
 }
 
   //UI Logic
   $(document).ready(function() {
-    $("#formOne").submit(function(){
-      const inputtedNumber = $("#number").val()
+    $("#formOne").submit(function(event){
+      event.preventDefault();
+      const inputtedNumber = parseInt($("#number").val());
+    //neighborHood(inputtedNumber);
     })
   })
