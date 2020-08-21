3b1a9f8de2ce711989eb4cd375c70b93475dafb8
jd <duverseaujohnny21@gmail.com>
2020-08-21 12:21:08 -0400

    Add my Bussiness logic and Ui Logic

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+  //Bussiness Logic
+function neighborHood() { 
+
+}
+
+  //UI Logic
+  $(document).ready(function() {
+    $("#formOne").submit(function(){
+      const inputtedNumber = $("#number").val()
+    })
+  })
