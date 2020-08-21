8000e8fbb927da8b441108138c623c3f97144743
jd <duverseaujohnny21@gmail.com>
2020-08-21 18:22:02 -0400

    add Ui logic

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@ const number = [];
 for (let i = 0; i <= hood; i++) {
  number.push(i.toString());
  if ((number[i]).match("3")) {
-  number[i] = ("Won't you be my neigbor...");
+  number[i] = ("Won't you be my neigbor?.");
  } else if ((number[i]).match("2")) {
  number[i] = ("Boop");
  } else if (number[i].match("1")) {
@@ -21,6 +21,10 @@ return number;
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
     event.preventDefault();
+    const inputtedNumber = $("#number").val();
+    let result = neighbor(inputtedNumber);
+    // console.log(result)
+    $("#output").text(result);
     
     
   });
