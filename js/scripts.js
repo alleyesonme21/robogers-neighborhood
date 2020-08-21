5d3935e8bf2623a45fb2094c3c9424665a7c2823
jd <duverseaujohnny21@gmail.com>
2020-08-21 18:06:03 -0400

    add the second condition

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,10 @@ for (let i = 0; i <= hood; i++) {
  number.push(i.toString());
  if ((number[i]).match("3")) {
   number[i] = ("Won't you be my neigbor...");
+ } else if ((number[i]).match("2")) {
+ number[i] = ("Boop");
  }
+
   
 }
 return number;
