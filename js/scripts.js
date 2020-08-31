f8c1647343de127e11e1f017419726ccbef83e10
jd <duverseaujohnny21@gmail.com>
2020-08-30 21:52:03 -0400

    Replace .toString to TL etc

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
   //Bussiness Logic
 function neighbor(hood) {
-  const number = [];
+  let number = [];
   for (let i = 0; i <= hood; i++) {
-    number.push(i.toString());
+    number.push(`${i}`);
     if ((number[i]).match("3")) {
       number[i] = ("Won't you be my neigbor?.");
     } else if ((number[i]).match("2")) {
@@ -16,7 +16,7 @@ function neighbor(hood) {
 return number;
 };
 
-  // Business logic
+  //UI logic
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
     event.preventDefault();
